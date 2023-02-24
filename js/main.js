a4c3f1edb681d6810dde3cdb3701e4c065a8f305
Alex Nunzi <alex.nunzi@hotmail.it>
2023-02-24 15:50:34 +0100

    Bonus 1/2: Aggiunto contenitore nel DOM tramite JS e sfruttando la logica precedentemente creata sono stati inseriti anche i contenitori dei dati generati dal ciclo for opportunamente stilizzati.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
+const mainDom = document.querySelector('main');
+const containerDom = document.createElement('div');
+
+containerDom.classList.add('container');
+
+mainDom.append(containerDom);
+
 for(let i=1; i <=100; i++){
+
+    let bgColor;
+    let elementData;
 
     if(i % 3 == 0 && i % 5 == 0){
         console.log(`${i} è divisibile per entrambi FizzBuzz`);
+        bgColor = 'bg-red';
+        elementData = 'FrizzBuzz';
     } else if(i % 5 == 0){
         console.log(`${i} é divisibile per 5 Buzz`);
+        bgColor = 'bg-yellow';
+        elementData = 'Buzz';
     } else if (i % 3 == 0){
         console.log(`${i} è divisibile per 3 Fizz`);
+        bgColor = 'bg-lightGreen';
+        elementData = 'Frizz';
     } else {
         console.log(i);
+        bgColor = 'bg-lightBlue';
+        elementData = i;
     }
 
+    const content = document.createElement('div');
+    content.classList.add(bgColor);
+    content.classList.add('square')
+    content.append(elementData);
+    containerDom.append(content);
 }
